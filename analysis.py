b63ce952b6c10e0a724ded1ede76b484361083a5
hintro32 <hintro32@gmail.com>
2023-09-07 14:27:24 -0400

    add comments

--- analysis.py
+++ analysis.py
@@ -1,5 +1,6 @@
 import pandas as pd
 
+# read World Develoment Indicators into df
 df = pd.read_csv('https://media.githubusercontent.com/media/nickeubank/MIDS_Data/master/World_Development_Indicators/wdi_small_tidy_2015.csv')
 
 print(df[['Mortality rate, infant (per 1,000 live births)', 'GDP per capita (constant 2010 US$)', 'Country Name']])
